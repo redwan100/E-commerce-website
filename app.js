f66e3c533147768bd3afc30ecdd8d041cb9a12e6
redwan100 <ridoneislam987@gmail.com>
2021-10-13 08:17:36 +0600

    ok everything

--- app.js
+++ app.js
@@ -1,57 +1,72 @@
-// ======================= MENU BUTTON TOGGLE SCRIPT ==================================
+// TODO: ============== NAV BAR TOGGLE ===============
 const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId);
-    const nav = document.getElementById(navId);
+    const toggle = document.querySelector(toggleId);
+    const nav = document.querySelector(navId);
 
-
-    if (toggle && nav) {
+    if (toggleId && navId) {
         toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
+            nav.classList.toggle('show');
         })
     }
 }
 
-showMenu('nav__toggle', 'nav__menu')
+showMenu('#nav__toggle', '#nav__menu');
 
-// ======= Remove Mobile ==========
+// TODO: ============= REMOVE MENU MOBILE RESPONSIVE =========
 const links = document.querySelectorAll('.nav__link');
-const navMenu = document.getElementById('nav__menu');
+const navMenu = document.querySelector('.nav__menu');
 
-function linkAction() {
+const linkAction = () => {
     navMenu.classList.remove('show');
 }
 
-links.forEach((link) => link.addEventListener('click', linkAction))
 
 
-// =========== scroll section active link ==================
+links.forEach((link) => {
+    link.addEventListener('click', linkAction)
+});
 
+
+// =========== NAV LINK ADD ANIMATION =================
+
+const navItem = document.querySelectorAll(".nav__item")
+navItem.forEach((link, index) => {
+    if (link.style.animation) {
+        link.style.animation = '';
+    } else {
+        link.style.animation = `navLinksFade 0.3s ease forwards ${index / 3 + 1}s`
+    }
+})
+
+// TODO: =========== SCROLL SECTIONS ACTIVE LINK ==============
 const sections = document.querySelectorAll('section[id]');
 
 window.addEventListener('scroll', scrollActive);
 
 function scrollActive() {
-    const scrollY = window.pageYOffset;
+    const scrollTop = window.pageYOffset;
+    
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id')
 
-    sections.forEach((section) => {
-        const sectionHeight = section.offsetHeight 
-        const sectionTop = section.offsetTop - 50;
-        const sectionId = section.getAttribute('id');
-
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
-            document.querySelector(".nav__menu a[href*=" + sectionId +"]").classList.add('active');
+        if (scrollTop > sectionTop && scrollTop <= sectionTop + sectionHeight) {
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add('active');
+            
         } else {
-            document.querySelector(".nav__menu a[href*=" + sectionId +"]").classList.remove('active');
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove('active');
+            
         }
     })
 }
 
 
-// =============== Header color change =======================
-
+// TODO: ================ SCROLL TO HEADER COLOR CHANGE =============
 window.addEventListener('scroll', () => {
-    const nav = document.getElementById('header');
-    if (this.scrollY >= 650) {
+    const nav = document.querySelector('#header');
+    const scrollY = window.scrollY;
+    if (scrollY > 650) {
         nav.classList.add('scroll__header');
     } else {
         nav.classList.remove('scroll__header');
@@ -60,48 +75,40 @@ window.addEventListener('scroll', () => {
 
 
 
- 
+// TODO: ================ CLICK TO TOP SCRIPT =============
 
 
-
-// ================== CLICK TO SCROLL TOP SCRIPT ===============================
-
-const topBtn = document.querySelector('.top__btn');
-console.log(topBtn);
-
-window.addEventListener("scroll", (e) => {
-  const scrollTop = window.scrollY;
-  if (scrollTop > 500) {
-    topBtn.classList.add('btn__active');
-  } else {
-    topBtn.classList.remove('btn__active');
-  }
+const topBtn = document.querySelector('.top-btn');
+topBtn.addEventListener('click', () => {
+    window.scrollTo(0,0)
 })
 
-topBtn.addEventListener('click', () => {
-    if (topBtn) {
-        scrollTo(0, 0);
+window.addEventListener('scroll', () => {
+    const progressLine = document.querySelector('.progress__line');
+    const progressBar = document.querySelector('.progress__bar');
+    const scrollTop = window.scrollY;
+
+    if (scrollTop > 50) {
+        progressBar.style.opacity = '1';
+    } else {
+        progressBar.style.opacity = '0';
+    }
+
+
+    // ======== CUSTOM PROGRESS BAR SCRIPT ==========
+    const totalHeight = document.body.scrollHeight - window.innerHeight;
+    const progressHeight = window.pageYOffset / totalHeight * 100;
+    progressLine.style.height = progressHeight + '%';
+
+    // ====== CLICK TO SCROLL TOP ========
+    if (scrollTop > 500) {
+        topBtn.classList.add('top__active');
+    } 
+    else {
+        topBtn.classList.remove('top__active');
     }
 })
 
 
 
-const subsBtn = document.querySelector('.subscribe__btn');
 
-subsBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    setTimeout(() => {
-        subsBtn.innerHTML = 'Done'+"\u00BB"
-    },1000)
-})
-
-
-// ==================== CUSTOM PROGRESS BAR SCRIPT ========================
-const progressBar = document.querySelector(".progressbar__line");
-console.log(progressBar);
-window.addEventListener('scroll', () => {
-    let totalHeight = document.body.scrollHeight - window.innerHeight;
-    let progress = (window.pageYOffset / totalHeight) * 100;
-    progressBar.style.height = progress + '%'
-    console.log(progress);
-})
